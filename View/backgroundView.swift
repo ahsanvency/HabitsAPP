f7de82bf4f6340b63f73df9170bae7f34ecba908
ahsanvency <35584044+ahsanvency@users.noreply.github.com>
2018-02-28 16:58:55 -0600

    Add files via upload

--- View/backgroundView.swift
+++ View/backgroundView.swift
@@ -13,5 +13,19 @@ class backgroundView: UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
         backgroundColor = seaFoamColor
+//
+//        let layer : CAGradientLayer = CAGradientLayer()
+//        layer.frame.size = self.frame.size
+//
+//        let color0 = UIColor(red:9/255, green:42/255, blue:48/255, alpha:0.5).cgColor
+//        let color1 = UIColor(red:200.0/255, green:200.0/255, blue: 200.0/255, alpha:0.1).cgColor
+//        let color2 = UIColor(red:150.0/255, green:150.0/255, blue: 150.0/255, alpha:0.1).cgColor
+//        let color3 = UIColor(red:100.0/255, green:100.0/255, blue: 100.0/255, alpha:0.1).cgColor
+//        let color4 = UIColor(red:50.0/255, green:50.0/255, blue:50.0/255, alpha:0.1).cgColor
+//        let color5 = UIColor(red:0.0/255, green:0.0/255, blue:0.0/255, alpha:0.1).cgColor
+//        let color6 = UIColor(red:5/255, green:28/255, blue:32/255, alpha:0.1).cgColor
+//
+//        layer.colors = [color0,color6]
+//        self.layer.insertSublayer(layer, at: 0)
     }
 }
--- View/fancyButton.swift
+++ View/fancyButton.swift
@@ -16,10 +16,10 @@ class fancyButton: UIButton {
         layer.shadowOpacity = 0.8;
         layer.shadowRadius = 5.0;
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0);
-        layer.cornerRadius = 8.0;
+        layer.cornerRadius = 20.0;
         layer.masksToBounds = true;
-        backgroundColor = satinColor
-        setTitleColor(seaFoamColor, for: .normal)
+        backgroundColor = blueColor
+        setTitleColor(satinColor, for: .normal)
     }
 
 }
