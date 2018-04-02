6c717a54d25eab71dfc67207bc683b0fa50c86e9
Ahsan Vency <ahsanvency@gmail.com>
2018-04-02 17:03:08 -0500

    Views resized

--- View/Other Views/roundedButton.swift
+++ View/Other Views/roundedButton.swift
@@ -8,14 +8,11 @@
 
 import UIKit
 
-class fancyButton: UIButton {
+class roundedButton: UIButton {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        layer.shadowOpacity = 0.8;
-        layer.shadowRadius = 5.0;
-        layer.shadowOffset = CGSize(width: 1.0, height: 1.0);
-        layer.cornerRadius = 25.0;
+        layer.cornerRadius = 5.0;
         layer.masksToBounds = true;
     }
 }
